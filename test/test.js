a7523838f3c2481748c7f47de8676da2318b3388
Ingvar Reverser <me@rreverser.com>
2013-06-28 17:50:05 +0300

    Simplified tests, added test for Bytes setter.

--- test/test.js
+++ test/test.js
@@ -97,6 +97,18 @@ function b() {
 	return new jDataView(arguments);
 }
 
+function compareInt64(value, expected) {
+	equal(Number(value), expected);
+}
+
+function compareBytes(value, expected) {
+	deepEqual(Array.prototype.slice.call(value), expected);
+}
+
+function compareWithNaN(value) {
+	ok(isNaN(value));
+}
+
 function module(name) {
 	name = engineName + (name ? ': ' + name : '');
 	QUnit.module.apply(this, arguments);
@@ -114,47 +126,41 @@ function testBounds(type) {
 	});
 }
 
-// getter = [value || checkFn, args, view]
+// getter = value || {value, check?, view?, args?}
 function testGetters(type, getters) {
 	test(type, function () {
 		for (var i = 0; i < getters.length; i++) {
 			var getter = getters[i];
 
-			if (!(getter instanceof Array)) {
-				getter = [getter];
+			if (typeof getter !== 'object') {
+				getter = {value: getter};
 			}
 
-			var value = getter[0],
-				args = getter[1] === undefined ? [] : getter[1] instanceof Array ? getter[1] : [getter[1]],
-				contextView = getter[2] || view,
-				check = value instanceof Function ? value : equal;
+			var args = getter.args || [],
+				contextView = getter.view || view,
+				check = getter.check || equal,
+				value = getter.value;
 
 			check(contextView['get' + type].apply(contextView, args), value);
 		}
 	});
 }
 
-function testForInt64(expected) {
-	return function (value) {
-		equal(Number(value), expected);
-	};
-}
-
-// setter = [value, args, getterArgs, checkFn]
+// setter = value || {value, args?, getterArgs?, check?}
 function testSetters(type, setters) {
 	test(type, function () {
 		for (var i = 0; i < setters.length; i++) {
 			var setter = setters[i];
 
-			if (!(setter instanceof Array)) {
-				setter = [setter];
+			if (typeof setter !== 'object') {
+				setter = {value: setter};
 			}
 
-			var value = setter[0],
-				args = setter[1] instanceof Array ? setter[1] : [setter[1] || 0],
+			var args = setter.args || [0],
 				offset = args[0],
-				getterArgs = setter[2] === undefined ? [offset] : setter[2] instanceof Array ? setter[2] : [setter[2]],
-				check = setter[3] || equal;
+				getterArgs = setter.getterArgs || [offset],
+				check = setter.check || equal,
+				value = setter.value;
 
 			view['set' + type].apply(view, [offset, value].concat(args.slice(1)));
 			check(view['get' + type].apply(view, getterArgs), value);
@@ -186,15 +192,19 @@ function testNextEngine() {
 		testBounds('Float32', 5);
 		testBounds('Float64', 1);
 
-		module('Value Read');
+		module('Value Read', {
+			teardown: function () {
+				view.seek(0);
+			}
+		});
 
 		testGetters('Bytes', [
-			[function (bytes) { deepEqual(Array.prototype.slice.call(bytes), [0xfe, 0xfd]) }, [2, 1]],
-			[function (bytes) { deepEqual(Array.prototype.slice.call(bytes), [0xfd, 0xfe, 0xff]) }, [3, 0, false]]
+			{args: [2, 1], value: [0xfe, 0xfd], check: compareBytes},
+			{args: [3, 0, false], value: [0xfd, 0xfe, 0xff], check: compareBytes}
 		]);
 
 		testGetters('Char', [
-			[chr(0xff), 0],
+			chr(0xff),
 			chr(0xfe),
 			chr(0xfd),
 			chr(0xfc),
@@ -205,11 +215,11 @@ function testNextEngine() {
 		]);
 
 		testGetters('String', [
-			[chr(0xff), [1, 0]],
-			[chr(0), [1, 5]],
-			[chr(1), [1, 7]],
-			[chr(127) + chr(0) + chr(1) + chr(65) + chr(66), 5, b(127, 0, 1, 65, 66)],
-			[chr(1092) + chr(1099) + chr(1074), [, , 'utf8'], b(0xd1, 0x84, 0xd1, 0x8b, 0xd0, 0xb2)]
+ 			{args: [1, 0], value: chr(0xff)},
+ 			{args: [1, 5], value: chr(0)},
+ 			{args: [1, 7], value: chr(1)},
+			{view: b(127, 0, 1, 65, 66), args: [5], value: chr(127) + chr(0) + chr(1) + chr(65) + chr(66)},
+			{view: b(0xd1, 0x84, 0xd1, 0x8b, 0xd0, 0xb2), args: [, , 'utf8'], value: chr(1092) + chr(1099) + chr(1074)}
 		]);
 
 		test('Big String', function () {
@@ -223,7 +233,7 @@ function testNextEngine() {
 		});
 
 		testGetters('Int8', [
-			[-1, 0],
+			-1,
 			-2,
 			-3,
 			-4,
@@ -234,7 +244,7 @@ function testNextEngine() {
 		]);
 
 		testGetters('Uint8', [
-			[255, 0],
+			255,
 			254,
 			253,
 			252,
@@ -245,75 +255,75 @@ function testNextEngine() {
 		]);
 
 		testGetters('Int16', [
-			[-257, 0],
-			[-514, 1],
-			[-771, 2],
-			[-1284, 3],
-			[250, 4],
-			[-17920, 5],
-			[442, 6]
+			{args: [0], value: -257},
+			{args: [1], value: -514},
+			{args: [2], value: -771},
+			{args: [3], value: -1284},
+			{args: [4], value: 250},
+			{args: [5], value: -17920},
+			{args: [6], value: 442}
 		]);
 
 		testGetters('Uint16', [
-			[65279, 0],
-			[65022, 1],
-			[64765, 2],
-			[64252, 3],
-			[250, 4],
-			[47616, 5],
-			[442, 6]
+			{args: [0], value: 65279},
+			{args: [1], value: 65022},
+			{args: [2], value: 64765},
+			{args: [3], value: 64252},
+			{args: [4], value: 250},
+			{args: [5], value: 47616},
+			{args: [6], value: 442}
 		]);
 
 		testGetters('Uint32', [
-			[4244504319, 0],
-			[4210884094, 1],
-			[16448765, 2],
-			[3120626428, 3],
-			[28967162, 4]
+			{args: [0], value: 4244504319},
+			{args: [1], value: 4210884094},
+			{args: [2], value: 16448765},
+			{args: [3], value: 3120626428},
+			{args: [4], value: 28967162}
 		]);
 
 		testGetters('Int32', [
-			[-50462977, 0],
-			[-84083202, 1],
-			[16448765, 2],
-			[-1174340868, 3],
-			[28967162, 4]
+			{args: [0], value: -50462977},
+			{args: [1], value: -84083202},
+			{args: [2], value: 16448765},
+			{args: [3], value: -1174340868},
+			{args: [4], value: 28967162}
 		]);
 
 		testGetters('Float32', [
-			[-1.055058432344064e+37, 0],
-			[-6.568051909668895e+35, 1],
-			[2.30496291345398e-38, 2],
-			[-0.0004920212086290121, 3],
-			[6.832701044000979e-38, 4],
-			[Infinity, , b(0x7f, 0x80, 0x00, 0x00)],
-			[-Infinity, , b(0xff, 0x80, 0x00, 0x00)],
-			[0, , b(0x00, 0x00, 0x00, 0x00)],
-			[function (value) { ok(isNaN(value)) }, , b(0xff, 0x80, 0x00, 0x01)]
+			{args: [0], value: -1.055058432344064e+37},
+			{args: [1], value: -6.568051909668895e+35},
+			{args: [2], value: 2.30496291345398e-38},
+			{args: [3], value: -0.0004920212086290121},
+			{args: [4], value: 6.832701044000979e-38},
+			{view: b(0x7f, 0x80, 0x00, 0x00), value: Infinity},
+			{view: b(0xff, 0x80, 0x00, 0x00), value: -Infinity},
+			{view: b(0x00, 0x00, 0x00, 0x00), value: 0},
+			{view: b(0xff, 0x80, 0x00, 0x01), check: compareWithNaN}
 		]);
 
 		testGetters('Float64', [
-			[2.426842827241402e-300, 0],
-			[Infinity, , b(0x7f, 0xf0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00)],
-			[-Infinity, , b(0xff, 0xf0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00)],
-			[0, , b(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00)],
-			[-0, , b(0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00)],
-			[1, , b(0x3f, 0xf0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00)],
-			[1.0000000000000002, , b(0x3f, 0xf0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01)],
-			[1.0000000000000004, , b(0x3f, 0xf0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02)],
-			[2, , b(0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00)],
-			[-2, , b(0xc0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00)],
-			[5e-324, , b(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01)],
-			[2.225073858507201e-308, , b(0x00, 0x0f, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff)],
-			[2.2250738585072014e-308, , b(0x00, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00)],
-			[1.7976931348623157e+308, , b(0x7f, 0xef, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff)],
-			[function (value) { ok(isNaN(value)) }, , b(0xff, 0xf0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01)]
+			{args: [0], value: 2.426842827241402e-300},
+			{view: b(0x7f, 0xf0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00), value: Infinity},
+			{view: b(0xff, 0xf0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00), value: -Infinity},
+			{view: b(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00), value: 0},
+			{view: b(0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00), value: -0},
+			{view: b(0x3f, 0xf0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00), value: 1},
+			{view: b(0x3f, 0xf0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01), value: 1.0000000000000002},
+			{view: b(0x3f, 0xf0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02), value: 1.0000000000000004},
+			{view: b(0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00), value: 2},
+			{view: b(0xc0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00), value: -2},
+			{view: b(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01), value: 5e-324},
+			{view: b(0x00, 0x0f, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff), value: 2.225073858507201e-308},
+			{view: b(0x00, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00), value: 2.2250738585072014e-308},
+			{view: b(0x7f, 0xef, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff), value: 1.7976931348623157e+308},
+			{view: b(0xff, 0xf0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01), check: compareWithNaN}
 		]);
 
 		testGetters('Int64', [
-			[testForInt64(-283686985483775), [0, false]],
-			[testForInt64(-2), , b(0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xfe)],
-			[testForInt64(4822678189205111), , b(0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77)]
+			{args: [0, false], value: -283686985483775, check: compareInt64},
+			{view: b(0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xfe), value: -2, check: compareInt64},
+			{view: b(0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77), value: 4822678189205111, check: compareInt64}
 		]);
 
 		module('Value Write', {
@@ -321,6 +331,11 @@ function testNextEngine() {
 				view.setBytes(0, dataBytes.slice(dataStart), true);
 			}
 		});
+
+		testSetters('Bytes', [
+			{args: [1], getterArgs: [2, 1], value: [0xfe, 0xfd], check: compareBytes},
+			{args: [0, false], getterArgs: [3, 0, false], value: [0xfd, 0xfe, 0xff], check: compareBytes}
+		]);
 
 		testSetters('Char', [
 			chr(0xdf),
@@ -330,9 +345,9 @@ function testNextEngine() {
 		]);
 
 		testSetters('String', [
-			[chr(1) + chr(2) + chr(3), 2, [3, 2]],
-			[chr(8) + chr(9), 1, [2, 1]],
-			[chr(1092) + chr(1099) + chr(1074), [0, 'utf8'], [6, 0, 'utf8']]
+			{args: [2], getterArgs: [3, 2], value: chr(1) + chr(2) + chr(3)},
+			{args: [1], getterArgs: [2, 1], value: chr(8) + chr(9)},
+			{args: [0, 'utf8'], getterArgs: [6, 0, 'utf8'], value: chr(1092) + chr(1099) + chr(1074)}
 		]);
 
 		testSetters('Int8', [
@@ -381,7 +396,7 @@ function testNextEngine() {
 			Infinity,
 			-Infinity,
 			0,
-			[NaN, , , function (value) { ok(isNaN(value)) }]
+			{value: NaN, check: compareWithNaN}
 		]);
 
 		testSetters('Float64', [
@@ -396,13 +411,13 @@ function testNextEngine() {
 			1,
 			1.0000000000000004,
 			-2,
-			[NaN, , , function (value) { ok(isNaN(value)) }]
+			{value: NaN, check: compareWithNaN}
 		]);
 
 		testSetters('Int64', [
-			[-283686985483775, , , testForInt64(-283686985483775)],
-			[-2, , , testForInt64(-2)],
-			[4822678189205111, , , testForInt64(4822678189205111)]
+			{value: -283686985483775, check: compareInt64},
+			{value: -2, check: compareInt64},
+			{value: 4822678189205111, check: compareInt64}
 		]);
 
 		test('slice', function () {
@@ -414,13 +429,13 @@ function testNextEngine() {
 			}
 
 			var pointerCopy = view.slice(1, 4);
-			deepEqual(Array.prototype.slice.call(pointerCopy.getBytes()), [0xfe, 0xfd, 0xfc]);
+			compareBytes(pointerCopy.getBytes(), [0xfe, 0xfd, 0xfc]);
 			pointerCopy.setChar(0, chr(1));
 			equal(view.getChar(1), chr(1));
 			pointerCopy.setChar(0, chr(0xfe));
 
 			var copy = view.slice(1, 4, true);
-			deepEqual(Array.prototype.slice.call(copy.getBytes()), [0xfe, 0xfd, 0xfc]);
+			compareBytes(copy.getBytes(), [0xfe, 0xfd, 0xfc]);
 			copy.setChar(0, chr(1));
 			notEqual(view.getChar(1), chr(1));
 		});
