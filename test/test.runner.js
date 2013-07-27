e5a5371012c2e6b90e31d94cc62a4b43bc633896
Ingvar Stepanyan <me@rreverser.com>
2013-07-27 11:57:04 +0300

    Changed Mocha reporter to "spec".

--- test/test.runner.js
+++ test/test.runner.js
@@ -1,8 +1,8 @@
 var Mocha = require('mocha');
 require('qunit-mocha-ui');
 
-var mocha = new Mocha({ui: 'qunit-mocha-ui'});
+var mocha = new Mocha({ui: 'qunit-mocha-ui', reporter: 'spec'});
 mocha.addFile('test.js');
 mocha.run(function(failures){
-  process.exit(failures);
+	process.exit(failures);
 });
