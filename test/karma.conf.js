6e08ef434f663bc1652f792ff603a5f4eb3ad7ab
Ingvar Stepanyan <me@rreverser.com>
2017-05-30 15:01:16 +0100

    Bump dependencies
     - Update tested Node.js versions for Travis.
     - Update most npm dependencies.
     - Add grunt-cli as explicit devDepdendency.
     - Add platform-dependant list of browsers for Karma testing.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,10 +1,24 @@
 module.exports = function (config) {
 	var CI = process.env.CI;
 
+	var browsers = ['PhantomJS'];
+
+	if (CI) {
+		browsers.push('Firefox');
+	} else {
+		browsers.push('ChromeCanary', 'FirefoxDeveloper');
+
+		if (process.platform === 'win32') {
+			browsers.push('IE', 'Edge');
+		} else if (process.platform === 'darwin') {
+			browsers.push('Safari');
+		}
+	}
+
 	config.set({
 		basePath: '..',
 		frameworks: ['mocha', 'chai'],
-		browsers: CI ? ['Firefox'] : ['Firefox', 'IE'],
+		browsers: browsers,
 		files: [
 			'dist/browser/jdataview.js',
 			'test/karma.mocha.conf.js',
