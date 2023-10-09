57f155524aca34a7cf7c8c3b5b02912145cba958
Zade Viggers <zade@viggers.net>
2023-10-09 17:07:44 +1300

    Add support for `SharedArrayBuffer`

--- src/helpers.js
+++ src/helpers.js
@@ -11,18 +11,27 @@ export function getCharCodes(string) {
 // mostly internal function for wrapping any supported input (String or Array-like) to best suitable buffer format
 export function wrapBuffer(buffer) {
 	switch (typeof buffer) {
-		case "number":
+		case "number": {
 			buffer = new Uint8Array(buffer).buffer;
 			break;
+		}
 
-		case "string":
+		case "string": {
 			buffer = getCharCodes(buffer).buffer;
 			break;
+		}
 
-		default:
-			if (!(buffer instanceof ArrayBuffer)) {
+		default: {
+			if (
+				!(
+					buffer instanceof ArrayBuffer ||
+					(typeof SharedArrayBuffer !== "undefined" &&
+						buffer instanceof SharedArrayBuffer)
+				)
+			) {
 				buffer = new Uint8Array(buffer).buffer;
 			}
+		}
 	}
 	return buffer;
 }
--- test/compat.test.js
+++ test/compat.test.js
@@ -1,14 +1,21 @@
 import { jDataView } from "../src/jdataview";
-import { describe, test } from "vitest";
+import { describe, it } from "vitest";
 import { compareBytes } from "./test-helpers";
+import { expect } from "vitest";
 
 describe("Compatibility", function () {
-	test("can be used in jDataView from Uint8Array::subarray", function () {
-		const bytes = [1, 2, 3],
-			offset = 1,
-			original = new Uint8Array(bytes),
-			view = new jDataView(original.subarray(offset));
+	it("Can be used in jDataView from Uint8Array::subarray", () => {
+		const bytes = [1, 2, 3];
+		const offset = 1;
+		const original = new Uint8Array(bytes);
+		const view = new jDataView(original.subarray(offset));
 
 		compareBytes(view.getBytes(), bytes.slice(offset));
+	});
+	it("Works with SharedArrayBuffer", () => {
+		const sharedBuffer = new SharedArrayBuffer(100);
+		const view = new jDataView(sharedBuffer);
+
+		expect(view.buffer).toBe(sharedBuffer);
 	});
 });
